b80c4dc472319bb8854154f74012978aa567e384
Craig Hammell <cilphex@gmail.com>
2013-04-03 21:09:03 -0400

    Badge option local storage

--- js/bin/options.js
+++ js/bin/options.js
@@ -1,7 +1,14 @@
+var Util = chrome.extension.getBackgroundPage().Util; 
+var Gox = chrome.extension.getBackgroundPage().Gox;
+
 angular.module('OptionsApp', [])
 	
 	.controller('OptionsCtrl', function OptionsCtrl($scope) {
 
-		$scope.test = "bacon";
+		$scope.display_badge = localStorage.getItem('display_badge') == 'true' ? true : false;
+
+		$scope.$watch('display_badge', function(new_val, old_val) {
+			localStorage.setItem('display_badge', new_val);
+		});
 
 	});
