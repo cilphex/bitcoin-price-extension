d9f8f9317f31d4a02b2c2607a1d4c9f143a9098c
Craig Hammell <cilphex@gmail.com>
2013-04-08 01:35:36 -0400

    reorg

--- js/bin/background.js
+++ js/bin/background.js
@@ -1,5 +1,5 @@
 // Utilities
-var Util = {
+var Util = util = {
 	devmode: false,
 	currencies: ['USD', 'AUD', 'CAD', 'CHF', 'CNY', 'DKK', 'EUR', 'GBP', 'HKD', 'JPY', 'NZD', 'PLN', 'RUB', 'SEK', 'SGD', 'THB'],
 	capitalize: function(text) {
@@ -10,51 +10,6 @@ var Util = {
 			console.log.apply(console, arguments);
 	}
 };
-
-/*
-var Blockchain = {
-	data:           {},
-	old_data:       {},
-	ticker_url:     'http://blockchain.info/ticker',
-	poll_id:        null,
-	poll_interval:  60000,
-
-	startPolling: function() {
-		this.pausePolling();
-		this.poll_id = setInterval(this.poll.bind(this), this.poll_interval);
-		this.poll();
-	},
-	pausePolling: function() {
-		clearTimeout(this.poll_id);
-	},
-	poll: function() {
-		if (!navigator.onLine)
-			return;
-		$.ajax({
-			url:     this.ticker_url,
-			success: this.poll_success.bind(this),
-			error:   this.poll_error.bind(this)
-		});
-	},
-	poll_success: function(data) {
-		for (var key in this.data) {
-			this.old_data[key] = this.data[key];
-			delete this.data[key];
-		}
-		for (var key in data) {
-			this.data[key] = data[key];
-		}
-		chrome.extension.sendMessage('update');
-	},
-	poll_error: function(jqXHR, textStatus, errorThrown) {
-		Util.log('BitAwesome polling error:', errorThrown);
-	},
-	initialize: function() {
-		this.startPolling();
-	}
-};
-*/
-
 
 
 var Settings = {
@@ -252,3 +207,56 @@ $(function() {
 	Settings.initialize();
 	Gox.initialize();
 });
+
+
+
+
+
+
+
+
+
+
+/*
+var Blockchain = {
+	data:           {},
+	old_data:       {},
+	ticker_url:     'http://blockchain.info/ticker',
+	poll_id:        null,
+	poll_interval:  60000,
+
+	startPolling: function() {
+		this.pausePolling();
+		this.poll_id = setInterval(this.poll.bind(this), this.poll_interval);
+		this.poll();
+	},
+	pausePolling: function() {
+		clearTimeout(this.poll_id);
+	},
+	poll: function() {
+		if (!navigator.onLine)
+			return;
+		$.ajax({
+			url:     this.ticker_url,
+			success: this.poll_success.bind(this),
+			error:   this.poll_error.bind(this)
+		});
+	},
+	poll_success: function(data) {
+		for (var key in this.data) {
+			this.old_data[key] = this.data[key];
+			delete this.data[key];
+		}
+		for (var key in data) {
+			this.data[key] = data[key];
+		}
+		chrome.extension.sendMessage('update');
+	},
+	poll_error: function(jqXHR, textStatus, errorThrown) {
+		Util.log('BitAwesome polling error:', errorThrown);
+	},
+	initialize: function() {
+		this.startPolling();
+	}
+};
+*/
